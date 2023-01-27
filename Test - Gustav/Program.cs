969156a520117f8a8d631245fa5eabc63b18433b
Elev <Elev@TEB125113>
2023-01-27 14:55:11 +0100

    Add project files.

---
+++ Test - Gustav/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TestGustav
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+
+            double sum = 0;
+            for (int i = 0; i < temperatur.Length; i++)
+            {
+                sum = sum + temperatur[i];
+            }
+            Console.WriteLine("Medeltemperaturen är:" + sum / temperatur.Length);
+
+        }
+    }
+}
